90d99e6a994d87b252ee4b3187a0128f81348015
JacobMJones <mrjacobtaiwa@gmail.com>
2018-05-26 16:45:18 -0400

    gives long and lat

--- public/tracks.js
+++ public/tracks.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
     console.log('in tracks start');
-    $('#status').text('i can change status');
+
+    if ("geolocation" in navigator) {
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            $('#status').text(position.coords.latitude + " " + position.coords.longitude);
+
+        });
+    } else {
+        $('#status').text('geolocation DOESNT exists');
+    }
 });
